533867345e36e81a77ca55c533aa542f2ffb0605
Abdulqadir Abuharrus <aabuharrus@gmail.com>
2022-11-17 09:56:37 -0800

    -Fix anchor tags in navbar accordingly. -Fix img tags in home.ejs accordingly. -Add getter for "gallery" page.

--- app.js
+++ app.js
@@ -1,17 +1,34 @@
 const express = require("express");
-
+const ejs = require("ejs");
 const app = express();
+const _ = require("lodash");
 
+//Set view engine EJS
+app.set('view engine', 'ejs');
+////////////////////////////////
 // EXPRESS' built-in body parser
 app.use(express.json());
 app.use(express.urlencoded({
   extended: true
 }));
+app.use(express.static("public"));
 
+//Getters for different pages.
 app.get("/", function(req, res){
-  res.send("Hello");
+  res.render("home");
 });
 
-app.listen(9000, function(){
-  console.log("Server started on port 9000.");
+app.get("/gallery", function(req, res){
+  res.render("gallery");
+});
+
+// TODO: Getter for speific album pages via Express Routing
+// app.get("/gallery/:albumName", function(req, res){
+// const reqTitle = _.lowerCase(req.params.albumName);
+// const albumTitle = _.lowerCase(...);
+// {...}
+// })
+
+app.listen(3000, function(){
+  console.log("Server started on port 3000.");
 });
