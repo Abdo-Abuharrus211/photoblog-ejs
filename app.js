ca7be547ef8a2199dae20cd194570cd5ae83701a
Abdulqadir Abuharrus <aabuharrus@gmail.com>
2022-12-12 15:26:25 -0800

    -Add server port detection

--- app.js
+++ app.js
@@ -76,6 +76,11 @@ app.get("/gallery/:albumName", function(req, res){
 });
 
 
-app.listen(3000, function(){
-  console.log("Server started on port 3000.");
+let port = process.env.PORT;
+if(port == null || port == ""){
+  port = 3000;
+}
+app.listen(port, function() {
+  console.log("Server started successfully.");
 });
+
